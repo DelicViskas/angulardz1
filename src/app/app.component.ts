e8ca27eb973a0f3657f8d6d1dd48d74cb0f7d533
Andrey Dyatlov <dag_freak@icloud.com>
2024-12-22 12:42:56 +0700

    novosibirsk-info

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CityInfoComponent } from './city-info/city-info.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CityInfoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
